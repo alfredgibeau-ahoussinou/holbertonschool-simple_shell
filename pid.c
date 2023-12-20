f59cdb8a8a1c7d3e49f50cb6f6801a9b56c6192f
alfredgibeau-ahoussinou <7550@holbertonstudents.com>
2023-12-20 14:34:22 +0100

    EncoreShell

--- pid.c
+++ pid.c
@@ -7,9 +7,9 @@
  */
 int shell1(void)
 {
-    pid_t my_pid;
+	pid_t my_pid;
 
-    my_pid = getpid();
-    printf("%u\n", my_pid);
-    return (0);
+	my_pid = getpid();
+	printf("%u\n", my_pid);
+	return (0);
 }
--- prompt.c
+++ prompt.c
@@ -1,22 +1,44 @@
-#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAX_LENGTH 1024
 
-int main(void) {
+int main (void) {
 	char command[MAX_LENGTH];
 
 	while (1) {
 		printf("cisfun$ ");
-		/* Affichage du prompt */
 
 		if (!fgets(command, MAX_LENGTH, stdin)) {
 			/* Lecture de la commande */
 			break;
 		}
 
+		/* Supprimer le saut de ligne à la fin de la commande */
+		command[strcspn(command, "\n")] = '\0';
+
 		if (strcmp(command, "exit") == 0) {
 			/* Commande pour quitter */
 			break;
+		} else if (strcmp(command, "ls") == 0) {
+			/* Commande ls */
+			system("ls");
+		} else if (strcmp(command, "pwd") == 0) {
+			/* Commande pwd */
+			system("pwd");
+		} else if (strcmp(command, "ls -l") == 0) {
+			/* Commande ls -l */
+			system("ls -l");
+		} else if (strcmp(command, "date") == 0) {
+			/* Commande date */
+			system("date");
+		} else if (strcmp(command, "echo") == 0) {
+			/* Commande echo */
+			system("echo qwerty");
+		} else {
+			/* Autres commandes */
+			printf("Commande non reconnue: %s\n", command);
 		}
 	}
 
