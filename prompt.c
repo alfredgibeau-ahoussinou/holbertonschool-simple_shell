5254ca46141406452b6cd14db0044c4db46cf55e
alfredgibeau-ahoussinou <7550@holbertonstudents.com>
2023-12-21 12:56:08 +0100

    SimpleShell

--- prompt.c
+++ prompt.c
@@ -1,46 +1,52 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "shell.h"
 
 #define MAX_LENGTH 1024
 
-int main (void) {
-	char command[MAX_LENGTH];
+int main(void) {
+    char command[MAX_LENGTH];
 
-	while (1) {
-		printf("cisfun$ ");
+    while (1) {
+        printf("simple_shell$");
 
-		if (!fgets(command, MAX_LENGTH, stdin)) {
-			/* Lecture de la commande */
-			break;
-		}
+        if (!fgets(command, MAX_LENGTH, stdin)) 
+		{
+            /* Lecture de la commande */
+            break;
+        }
 
-		/* Supprimer le saut de ligne à la fin de la commande */
-		command[strcspn(command, "\n")] = '\0';
+        /* Supprimer le saut de ligne à la fin de la commande */
+        command[strcspn(command, "\n")] = '\0';
 
-		if (strcmp(command, "exit") == 0) {
-			/* Commande pour quitter */
-			break;
-		} else if (strcmp(command, "ls") == 0) {
-			/* Commande ls */
-			system("ls");
-		} else if (strcmp(command, "pwd") == 0) {
-			/* Commande pwd */
-			system("pwd");
-		} else if (strcmp(command, "ls -l") == 0) {
-			/* Commande ls -l */
-			system("ls -l");
-		} else if (strcmp(command, "date") == 0) {
-			/* Commande date */
-			system("date");
-		} else if (strcmp(command, "echo") == 0) {
-			/* Commande echo */
-			system("echo /bin/ls");
-		} else {
-			/* Autres commandes */
-			printf("Commande non reconnue: %s\n", command);
-		}
-	}
+        if (strcmp(command, "exit") == 0) 
+		{
+            /* Commande pour quitter */
+            break;
+        } else if (strcmp(command, "ls") == 0) 
+		{
+            /* Commande ls */
+            system("ls");
+        } else if (strcmp(command, "pwd") == 0) 
+		{
+            /* Commande pwd */
+            system("pwd");
+        } else if (strncmp(command, "ls -l", 5) == 0) 
+		{
+            /* Commande ls -l */
+            system("ls -l");
+        } else if (strcmp(command, "date") == 0) 
+		{
+            /* Commande date */
+            system("date");
+        } else if (strcmp(command, "echo") == 0)
+		{
+            /* Commande echo */
+            system("echo /bin/ls");
+        } else 
+		{
+            /* Autres commandes */
+            printf("Commande non reconnue: %s\n", command);
+        }
+    }
 
-	return 0;
+    return (0);
 }
--- simple_shell.c
+++ simple_shell.c
@@ -6,11 +6,22 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-void display_prompt() {
-	printf("cisfun$ ");
+/**
+ * Display a prompt and wait for the user to type a command. A command line always ends with a new line. The prompt is displayed again each time a command has been executed.
+ * he command lines are simple, no semicolons, no pipes, no redirections or any other advanced features.
+ * The command lines are made only of one word. No arguments will be passed to programs.
+ * If an executable cannot be found, print an error message and display the prompt again.
+ * Handle errors.
+ * You have to handle the “end of file” condition (Ctrl+D)
+**/
+
+void display_prompt() 
+{
+	printf("simple_shell$");
 }
 
-void run_command(char *command) {
+void run_command(char *command) 
+{
 	pid_t pid = fork();
 	if (pid == 0) {
 		char *args[1024];  /* Tableau pour stocker les arguments */
@@ -18,32 +29,39 @@ void run_command(char *command) {
 
 		/* Utilisation de strtok pour extraire les arguments */
 		char *token = strtok(command, " \t\n");
-		while (token != NULL && i < sizeof(args) / sizeof(args[0]) - 1) {
+		while (token != NULL && i < sizeof(args) / sizeof(args[0]) - 1) 
+		{
 			args[i++] = token;
 			token = strtok(NULL, " \t\n");
 		}
 
 		args[i] = NULL;  /* Dernier élément du tableau doit être NULL */
 
-		if (execvp(args[0], args) == -1) {
+		if (execvp(args[0], args) == -1)
+		{
 			perror("simple_shell");
 		}
 		exit(EXIT_FAILURE);
-	} else if (pid < 0) {
+	} else if (pid < 0)
+	{
 		perror("simple_shell");
-	} else {
+	} else
+	{
 		int status;
 		waitpid(pid, &status, WUNTRACED);
 	}
 }
 
-int shell3() {
+int simple() 
+{
 	char command[1042];
-	while (1) {
+	while (1)
+	{
 		display_prompt();
-		if (fgets(command, sizeof(command), stdin) == NULL) {
+		if (fgets(command, sizeof(command), stdin) == NULL)
+		 {
 			break;
-		}
+		 }
 		command[strcspn(command, "\n")] = '\0';
 		run_command(command);
 	}
